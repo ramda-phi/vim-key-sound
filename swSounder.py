fee0671f2f5bfc0e6cc145349b2e2c5d88321375
yago <kennedy.icu@gmail.com>
2012-10-19 22:35:26 +0900

    add some sound src, and a split code

---
+++ swSounder.py
@@ -0,0 +1,36 @@
+import pyo
+import sys
+import time
+
+
+# 'SinWaveSounder' class generates sound file with input character(key-code)
+class SinWaveSounder:
+    def __init__(self, path):
+        self.path = path
+        self.scale = 2.0 ** (1 / 12.0)
+        self.freq = [0] * 28
+        for i in range(28):
+            self.freq[i] = 220 * (self.scale ** i)
+        self.server = pyo.Server(audio='coreaudio')
+        # if you don't use mac, 'pyo.Server()'
+        self.server.boot()
+        self.server.start()
+
+    def sound(self, ccode, sec):
+        t = pyo.HarmTable([1, 0, 0, .2, 0, 0, 0, .1, 0, 0, .05])
+        #t = pyo.HarmTable([1, 0, .33, 2, 0, .143, 0, .111])
+        amp = pyo.Fader(fadein=.01, fadeout=.07, dur=sec, mul=.2).play()
+        osc = pyo.Osc(t,
+                freq=[self.freq[ccode], self.freq[ccode] + 1],
+                mul=amp).out()
+        rec = pyo.Record(osc,
+                filename=self.path + "/" + sys.argv[1] + ".wav",
+                fileformat=0,
+                sampletype=1)
+        clean = pyo.Clean_objects(sec, rec)
+        clean.start()
+        time.sleep(sec)
+
+    def random_sound(self, seed, sec):
+        ccode = (ord(seed) + 15) % len(self.freq)
+        self.sound(ccode, sec)
--- vim-key-sound.py
+++ vim-key-sound.py
@@ -1,49 +1,7 @@
-# coding: utf-8
-
-#"========================================================================="
-#
-#   vim-key-sound.py
-#
-#   'vim-key-sound.py' is forked from 
-#
-#   ✘╹◡╹✘  音の鳴るエディタことVim
-#   http://r7kamura.hatenablog.com/entry/2012/10/09/155041
-#
-#   ssig33.com 音が鳴るエディタ軽量化
-#   http://ssig33.com/text/音が鳴るエディタ軽量化
-#
-#   [Requirements]
-#    to use 'vim-key-sound.py', you need
-#
-#       portaudio
-#       pyaudio
-#       pyo
-#       vimproc
-#
-#    Sorry, I verified this code only on Mac OS X Lion 10.7.5, python 2.7.3
-#
-#   [to Use]
-#
-#    set 'vks/tmp' in your home directory or rewrite variable 'path'
-#
-#       path = any_dir/you_set
-#
-#    and add this code in your .vimrc
-#
-#       autocmd CursorMovedI * :call vimproc#system_bg("python " . path/to/vim-key-sound/vim-key-sound.py " .getline('.')[col('.')-2])
-#
-#   [Known Bugs & todo]
-#
-#   1. No exception handling ['] in vimproc
-#   2. There are only one sound set. some snd set will be added
-#   3. without 'pyo' version
-#   4. and more
-#
-#"========================================================================="
-
 import os
 import sys
 import time
+import swSounder
 
 
 # please start with soundfile(.wav)
@@ -59,40 +17,10 @@ else:
     os.makedirs(path)
 
 
-# 'SinWaveSounder' class generates sound file with input character(key-code)
-class SinWaveSounder:
-    def __init__(self):
-        self.scale = 2.0 ** (1 / 12.0)
-        self.freq = [0] * 28
-        for i in range(28):
-            self.freq[i] = 220 * (self.scale ** i)
-        self.server = pyo.Server(audio='coreaudio')
-        # if you don't use mac, 'pyo.Server()'
-        self.server.boot()
-        self.server.start()
-
-    def sound(self, ccode, sec):
-        t = pyo.HarmTable([1, 0, 0, .2, 0, 0, 0, .1, 0, 0, .05])
-        #t = pyo.HarmTable([1, 0, .33, 2, 0, .143, 0, .111])
-        amp = pyo.Fader(fadein=.01, fadeout=.07, dur=sec, mul=.2).play()
-        osc = pyo.Osc(t,
-                freq=[self.freq[ccode], self.freq[ccode] + 1],
-                mul=amp).out()
-        rec = pyo.Record(osc,
-                filename=path + "/" + sys.argv[1] + ".wav",
-                fileformat=0,
-                sampletype=1)
-        clean = pyo.Clean_objects(sec, rec)
-        clean.start()
-        time.sleep(sec)
-
-    def random_sound(self, seed, sec):
-        ccode = (ord(seed) + 15) % len(self.freq)
-        self.sound(ccode, sec)
-
-
 sec = .1
 filename = path + "/" + sys.argv[1] + ".wav"
+#print '[' + sys.argv[1] + ']'
+#print ord(sys.argv[1])
 
 if os.path.exists(filename):
     import pyaudio
@@ -115,12 +43,11 @@ if os.path.exists(filename):
     stream.stop_stream()
     stream.close()
     f.close()
-    p.termionate()
+    p.terminate()
 
 # if there are no sound source, create sound source
 else:
-    import pyo
-    sounder = SinWaveSounder()
+    sounder = swSounder.SinWaveSounder(path)
     sounder.random_sound(sys.argv[1], sec)
     sounder.server.stop()
     time.sleep(.25)
